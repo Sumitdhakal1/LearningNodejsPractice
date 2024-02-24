0001d05ef4dcdb2eb866f590ec3958cda5f51d12
Sumit Dhakal <sumit@Sumits-MacBook-Pro.local>
2024-02-24 17:49:46 +0545

    server

---
+++ Nodejs/app.js
@@ -0,0 +1,58 @@
+// http module
+const http = require('http');
+
+const fs = require('fs');
+
+// function rqListener(req, res){
+
+// }
+
+// http.createServer(rqListener);
+
+
+
+// http.createServer(function(req, res){
+
+// })
+
+
+const server = http.createServer((req, res) => {   
+    // console.log(req.url, req.method, req.headers);
+    // process.exit();
+    const url = req.url;
+    const method = req.method
+    if (url === '/') {
+        res.write('<html>')
+        res.write('<head><title>enter message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk)
+            body.push(chunk);
+        });
+            
+          req.on('end', ()=>{
+             const parseBody =Buffer.concat(body).toString();
+             const message = parseBody.split('=')[1];
+             fs.writeFileSync('message.txt', message)
+            //  console.log(parseBody)
+          });
+        fs.writeFileSync('message.txt', 'dummy')
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end()
+    }
+    res.setHeader('Content-type', 'text/html')
+    res.write('<html>')
+    res.write('<head><title>my first heading</title></head>')
+    res.write('<body><h1>hello from my node</h1></body>')
+    res.write('</html>')
+    res.end();
+
+});
+
+server.listen(3000)
